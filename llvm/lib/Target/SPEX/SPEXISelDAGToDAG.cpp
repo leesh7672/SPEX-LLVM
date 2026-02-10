cd91ca44cfb63bf39ec095cab52b5e6f8b187162
Seho Lee <leesh7672@gmail.com>
2026-02-10 12:11:22 +0900

    [SPEX] Repair selections

--- llvm/lib/Target/SPEX/SPEXISelDAGToDAG.cpp
+++ llvm/lib/Target/SPEX/SPEXISelDAGToDAG.cpp
@@ -132,6 +132,53 @@ static unsigned pickMovR(unsigned Bits){
       return SPEX::MOVMOV32_R;
     case 64:
       return SPEX::MOVMOV64_R;
+    default:
+      llvm_unreachable("bad mov width");
+  }
+}
+
+static unsigned pickPseudoSETCC(unsigned Bits){
+  switch (Bits) {
+    case 8:
+      return SPEX::PSEUDO_SETCC8;
+    case 16:
+      return SPEX::PSEUDO_SETCC16;
+    case 32:
+      return SPEX::PSEUDO_SETCC32;
+    case 64:
+      return SPEX::PSEUDO_SETCC64;
+    default:
+      llvm_unreachable("bad SETCC width");
+  }
+}
+
+static unsigned pickPseudoSELECT(unsigned Bits){
+  switch (Bits) {
+    case 8:
+      return SPEX::PSEUDO_SELECT8;
+    case 16:
+      return SPEX::PSEUDO_SELECT16;
+    case 32:
+      return SPEX::PSEUDO_SELECT32;
+    case 64:
+      return SPEX::PSEUDO_SELECT64;
+    default:
+      llvm_unreachable("bad SELECT width");
+  }
+}
+
+static unsigned pickPseudoSELECT_CC(unsigned Bits){
+  switch (Bits) {
+    case 8:
+      return SPEX::PSEUDO_SELECT_CC8;
+    case 16:
+      return SPEX::PSEUDO_SELECT_CC16;
+    case 32:
+      return SPEX::PSEUDO_SELECT_CC32;
+    case 64:
+      return SPEX::PSEUDO_SELECT_CC64;
+    default:
+      llvm_unreachable("bad SELECT_CC width");
   }
 }
 
@@ -563,6 +610,58 @@ void SPEXDAGToDAGISel::Select(SDNode *Node) {
     SDValue Chain = Node->getOperand(0);
     SDNode *N = CurDAG->getMachineNode(SPEX::LWAKE, DL, MVT::Other, Chain);
     ReplaceNode(Node, N);
+    return;
+  }
+  case ISD::SETCC:
+  {
+    SDLoc DL(Node);
+    SDValue LHS = Node->getOperand(0);
+    SDValue RHS = Node->getOperand(1);
+    auto *CCN = cast<CondCodeSDNode>(Node->getOperand(2));
+    ISD::CondCode CC = CCN->get();
+
+    unsigned CmpBits = LHS.getValueType().getSizeInBits();
+    unsigned Op = pickPseudoSETCC(CmpBits);
+
+    SDValue TCC = CurDAG->getTargetConstant((int)CC, DL, MVT::i32);
+    SDNode *Res = CurDAG->getMachineNode(Op, DL, Node->getValueType(0), {LHS, RHS, TCC});
+
+    ReplaceNode(Node, Res);
+
+    return;
+  }
+  case ISD::SELECT: {
+    SDLoc DL(Node);
+    SDValue Cond = Node->getOperand(0);
+    SDValue TVal = Node->getOperand(1);
+    SDValue FVal = Node->getOperand(2);
+
+    unsigned CmpBits = Node->getValueType(0).getSizeInBits();
+    unsigned Op = pickPseudoSELECT(CmpBits);
+
+    SDNode *Res = CurDAG->getMachineNode(Op, DL, Node->getValueType(0), {Cond, TVal, FVal});
+
+    ReplaceNode(Node, Res);
+
+    return;
+  }
+  case ISD::SELECT_CC: {
+    SDLoc DL(Node);
+    SDValue LHS = Node->getOperand(0);
+    SDValue RHS = Node->getOperand(1);
+    SDValue TVal = Node->getOperand(2);
+    SDValue FVal = Node->getOperand(3);
+    auto *CCN = cast<CondCodeSDNode>(Node->getOperand(4));
+    ISD::CondCode CC = CCN->get();
+
+    unsigned CmpBits = LHS.getValueType().getSizeInBits();
+    unsigned Op = pickPseudoSELECT_CC(CmpBits);
+
+    SDValue TCC = CurDAG->getTargetConstant((int)CC, DL, MVT::i32);
+    SDNode *Res = CurDAG->getMachineNode(Op, DL, Node->getValueType(0), {LHS, RHS, TVal, FVal, TCC});
+
+    ReplaceNode(Node, Res);
+
     return;
   }
   }
